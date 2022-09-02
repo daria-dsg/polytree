b0551a8704bf8a0b6bb9ac49b59d484e63cebd9a
daria.dsg <daria.ui.ux@gmail.com>
2022-09-02 18:53:37 +0200

    Reassigning the parent

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,6 @@
+
+require "byebug"
+
 class PolyTreeNode
 
     attr_reader :value, :children, :parent
@@ -6,6 +9,24 @@ class PolyTreeNode
         @parent = nil
         @children = []
         @value = value
+    end
+
+    def parent=(parent)
+        #does not add the child twice 
+        return  if self.parent == parent 
+
+        # remove node from the previous parent
+        @parent.children.reject! { |child| child == self} if self.parent
+        
+        # reassign parent and add the node to his children
+        @parent = parent
+        @parent.children << self unless self.parent.nil?
+    end
+
+    def inspect
+        {parent: parent,
+         children: children,
+         value: value}.inspect
     end
 
 end
